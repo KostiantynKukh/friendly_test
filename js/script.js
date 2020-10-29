ecf5b750e8d0aed55ef329dccd2e4e2b1d9f828e
KostiantynKukh <kkuh1993@gmail.com>
2020-10-29 13:03:24 +0200

    made all sections

--- js/script.js
+++ js/script.js
@@ -1,12 +1,50 @@
-let navBar = document.querySelector('.nav-bar'),
-nav = document.querySelector('.nav');
+//burger
+$(document).ready( function () {
+  let navBar = document.querySelector('.nav-bar'),
+  nav = document.querySelector('.nav');
 
-const toggler = function () {
-  navBar.classList.toggle('nav-bar__active');
-  nav.classList.toggle('nav__active');
-}
+  const toggler = function () {
+    navBar.classList.toggle('nav-bar__active');
+    nav.classList.toggle('nav__active');
+  }
+  navBar.addEventListener('click', toggler);
+  nav.addEventListener('click', toggler);
 
-navBar.addEventListener('click', toggler);
-nav.addEventListener('click', toggler);
-
-
+  $('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },    
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: false
+        }
+      }   
+    ]
+  });
+}  
+)
